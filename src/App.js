5d89098a3d5eaf2589d4a0f5e1fb85f44dad2b19
Anis Islam <manisul1997bd@gmail.com>
2021-05-31 17:31:03 +0600

    details page ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+
+import React from "react";
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-// export const GlobalContext = createContext({});
+import Header from './components/Header';
+import ProductDetails from './components/ProductDetails';
+import ProductListing from './components/ProductListing';
 
 function App() {
-  // const [signedInUser, setsignedInUser] = useState({});
   return (
-    <h1>hello</h1>
+    <div className="App">
+
+      <Router>
+      <Header />
+
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
+
+    </div>
   );
 }
 
